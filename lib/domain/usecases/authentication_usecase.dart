2afc3089be272759b448d224a390f8867ca12ccf
Edvaldo <35712346+wsy152@users.noreply.github.com>
2023-02-20 23:06:56 -0300

    AuthenticParams commit

--- lib/domain/usecases/authentication_usecase.dart
+++ lib/domain/usecases/authentication_usecase.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:clean_architecture_tdd/domain/entities/account_entity.dart';
 
 abstract class AuthenticationUseCase {
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_tdd/domain/usecases/authentication_usecase.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 
@@ -9,13 +10,14 @@ class RemouteAuthentication {
 
   RemouteAuthentication({required this.httpClient, required this.url});
 
-  Future<void>? auth() async {
-    await httpClient.request(url: url,method: 'POST');
+  Future<void>? auth(AuthenticationParams params) async {
+    final body = {'email': params.email,'password':params.password};
+    await httpClient.request(url: url,method: 'POST',body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url,required String method});
+  Future<void>? request({required String url,required String method,Map body});
 }
 
 class HttpClientMock extends Mock implements HttpClient {}
@@ -28,13 +30,14 @@ void main() {
   setUp((){
      httpClient = HttpClientMock();
      url = faker.internet.httpUrl();
-     sut = RemouteAuthentication(httpClient: httpClient, url: url);
+     sut = RemouteAuthentication(httpClient: httpClient, url: url,);
   });
   
   test('Should call HttpClient with correct URL', () async {
+    final params = AuthenticationParams(email: faker.internet.email(), password: faker.internet.password());
 
-    await sut.auth();
+    await sut.auth(params);
 
-    verify(httpClient.request(url: url,method:'POST'));
+    verify(httpClient.request(url: url,method:'POST',body: {'email':params.email,'password':params.password}));
   });
 }
